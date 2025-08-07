1b64f9106b346ac364db7a3e38896fe65ee17773
Thanh Nhan <bthanhnhan123@gmail.com>
2025-08-07 14:51:14 +0700

    update

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from employee.models import Employee
 
 # Create your views here.
+def employee_detial(request , pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    return HttpResponse(employee)
